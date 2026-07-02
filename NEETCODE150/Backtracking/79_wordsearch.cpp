b0f8155d7408b76259e55baf9cfedb7487d0353a
32bikram <bikramsarkareveland@gmail.com>
2026-07-02 17:41:31 +0000

    2nd july

---
+++ NEETCODE150/Backtracking/79_wordsearch.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool ans = false;
+    void back(vector<vector<char>>& board, string word, int x, int y,
+                                                        int curr_ind){
+        if(curr_ind == word.size()-1 && word[curr_ind] == board[x][y]){
+            ans = true;
+            return;
+        }
+
+        if(word[curr_ind] == board[x][y]){
+            board[x][y] = '#';
+            curr_ind++;
+            if(y-1>=0 && board[x][y-1]==word[curr_ind]){
+                back(board, word, x, y-1, curr_ind);
+            }
+            if(y+1<board[0].size() && board[x][y+1]==word[curr_ind]){
+                back(board, word, x, y+1, curr_ind);
+            }
+            if(x-1>=0 && board[x-1][y]==word[curr_ind]){
+                back(board, word, x-1, y, curr_ind);
+            }
+            if(x+1<board.size() && board[x+1][y]==word[curr_ind]){
+                back(board, word, x+1, y, curr_ind);
+            }
+            board[x][y] = word[curr_ind-1];
+            return;
+        }
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int i = 0, j = 0, curr_ind = 0;
+        for(int i = 0; i<board.size(); i++){
+            for(int j = 0; j<board[0].size(); j++){
+                if(board[i][j]==word[0]){
+                    back(board, word, i, j, curr_ind);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
