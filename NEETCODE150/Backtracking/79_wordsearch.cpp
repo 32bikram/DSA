6c5f597b7952d17b0e68b55a58c2afc66d738f57
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-07-03 23:14:48 +0530

    Enhance comments in word search backtracking function
    Added comments to clarify the backtracking logic and character marking in the word search algorithm.

--- NEETCODE150/Backtracking/79_wordsearch.cpp
+++ NEETCODE150/Backtracking/79_wordsearch.cpp
@@ -10,20 +10,26 @@ public:
 
         if(word[curr_ind] == board[x][y]){
             board[x][y] = '#';
+            //marking the character as used like for aba same characters could be used twice
             curr_ind++;
             if(y-1>=0 && board[x][y-1]==word[curr_ind]){
                 back(board, word, x, y-1, curr_ind);
             }
+            
             if(y+1<board[0].size() && board[x][y+1]==word[curr_ind]){
                 back(board, word, x, y+1, curr_ind);
             }
+            
             if(x-1>=0 && board[x-1][y]==word[curr_ind]){
                 back(board, word, x-1, y, curr_ind);
             }
+            
             if(x+1<board.size() && board[x+1][y]==word[curr_ind]){
                 back(board, word, x+1, y, curr_ind);
             }
+            
             board[x][y] = word[curr_ind-1];
+            //restoring the board
             return;
         }
     }
