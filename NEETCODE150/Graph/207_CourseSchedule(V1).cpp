f7d0b263b2f3728bf694f36c80a81c021ef36cc1
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-07-14 23:10:47 +0530

    Implement Course Schedule solution using DFS

---
+++ NEETCODE150/Graph/207_CourseSchedule(V1).cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool DFS(pair<int, vector<int>> &node, vector<pair<int, vector<int>>> &graph){
+        if(node.first == 0) return true;    //no preq
+        if(node.first == -2) return false;   //deadlock
+        node.first = -2;
+        for(int i = 0; i<node.second.size(); i++){
+            if(!DFS(graph[node.second[i]], graph)) return false;
+        }
+        node.first = 0;
+        return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<pair<int, vector<int>>> graph (numCourses);
+        for(int i = 0; i<prerequisites.size(); i++){
+            graph[prerequisites[i][0]].first = -1;
+            graph[prerequisites[i][0]].second.push_back(prerequisites[i][1]);
+        }
+        for(int i = 0; i<graph.size(); i++){
+            if(!DFS(graph[i], graph)) return false;
+        }
+        return true;
+    }
+};
