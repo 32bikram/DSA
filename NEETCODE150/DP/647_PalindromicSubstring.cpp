561cfb5076107362df0573f7f8e577c153c3ce74
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-07-31 20:43:27 +0530

    Add function to count palindromic substrings
    Implement a method to count palindromic substrings in a given string using a two-pointer approach.

--- NEETCODE150/DP/647_PalindromicSubstring.cpp
+++ NEETCODE150/DP/647_PalindromicSubstring.cpp
@@ -1,1 +1,31 @@
 
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+        int l = 0, r = 0; 
+
+        for(int i = 0; i<s.size(); i++){
+
+            l = i-1, r = i+1;
+            while(r<s.size() && l>=0){
+                if(s[l]==s[r]){
+                    ans++;
+                    l--; r++;
+                }
+                else break;
+            }
+
+            l = i-1, r = i;
+            while(r<s.size() && l>=0){
+                if(s[l]==s[r]){
+                    ans++;
+                    l--; r++;
+                }
+                else break;
+            }
+        }
+        ans += s.size(); //single chars
+        return ans;
+    }
+};
