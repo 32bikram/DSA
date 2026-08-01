f5fdacb78e08a710f06815044af0c2e5587b214a
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-08-01 23:52:24 +0530

    Refactor numDecodings with recursive backtracking

--- NEETCODE150/DP/91_DecodeWays.cpp
+++ NEETCODE150/DP/91_DecodeWays.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        vector<int> dp(s.size());
-        bool flag = false;
 
-        if(s[0] == '0') return 0;
-        else dp[0] = 1;
-        if(s[1]=='0') dp[1] = dp[0];
-        else dp[1] = dp[0]+2; //or akta, ager tar akta, pair akta;
+    int back(string &s, int idx, vector<int> &dp){
+        if(idx >= s.size()) return 1;
+        if(s[idx]=='0') return 0;
+        if(dp[idx]!=-1) return dp[idx]; 
+        int ans = 0;
 
-        for(int i = 2; i<s.size(); i++){
-            if(s[i]==0) return 0;
-
-            if(i+1<s.size() && s[i+1]!=0){
-                if(flag == true){
-                    dp[i] = dp[i-1]+2; //one of its own, other for pair
-                    flag = false;
-                }
-                else{
-                    dp[i] = dp[i-1]+1;
-                    flag = true;
-                }
+        if(idx+1<s.size() && s[idx+1]=='0'){
+            if((s[idx]-'0')*10 + s[idx+1]-'0' <=26){
+                ans += back(s, idx+2, dp);
             }
-
-            else if(i+1<s.size() && s[i+1]==0){
-                if((s[i]-'0')*10 + s[i+1] - '0' <= 26){
-                    dp[i] = dp[i-1]; // 0 er ager tar jnno
-                    dp[i+1] = dp[i]+1; //0 er jnno
-                    flag = false; //since o zero r sathe pair banai nise akhon abar notun kore suru
-                    i++; //0 skip
-                }
-                else return 0;
-            }
-
-            else if(i+1 == s.size()){
-                if(flag == false) dp[i] = dp[i-1] + 1;
-                else dp[i] = dp[i-1]+2; //one of its own, other for pair
-            }
+            else return 0;
         }
-        return dp[s.size()-1];
+        else{
+            if(idx+1<s.size() && (s[idx]-'0')*10 + s[idx+1]-'0' <=26 ){
+                ans += back(s, idx+2, dp);
+            }
+            ans += back(s, idx+1, dp);
+        }
+        dp[idx] = ans;
+        return ans;
+    } 
+    int numDecodings(string s) {
+        vector<int> dp(s.size(), -1);
+        if(s[0]=='0') return 0;
+        return back(s, 0, dp);
     }
 };
