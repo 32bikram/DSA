8accd8d846887727baaa83aebaa43e0011aec8e7
Bikram Sarkar <bikramsarkareveland@gmail.com>
2026-09-26 00:01:01 +0530

    Update and rename InsertIntervel.cpp to 57_InsertIntervel.cpp

--- NEETCODE150/Greedy/57_InsertIntervel.cpp
+++ NEETCODE150/Greedy/57_InsertIntervel.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int new_left = newInterval[0], new_right = newInterval[1];
+        int new_left = newInterval[0], new_right = newInterval[1], i = 0;
         vector<vector<int>> ans;
         while(i<intervals.size() && intervals[i][1]<new_left){ //no overlap
             ans.push_back(intervals[i]);
@@ -10,10 +10,12 @@ public:
         while(i<intervals.size() && intervals[i][0]<=new_right){ //overlap starts till overlap persists
             new_left = min(new_left, intervals[i][0]);
             new_right = max(new_right, intervals[i][1]);
+            i++;
         }
         ans.push_back({new_left,new_right});
         while(i<intervals.size()){
             ans.push_back(intervals[i]);
+            i++;
         }
         return ans;
     }
